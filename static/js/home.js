8a95d60c4cc1024360789d9ac4c83ca1c3b524d2
JeongHawook <saroball3@gmail.com>
2023-03-29 11:50:26 +0900

    1

---
+++ static/js/home.js
@@ -0,0 +1,139 @@
+$(document).ready(function () {
+  let url = window.location.href;
+  let idResult = url.substring(url.lastIndexOf("/") + 1);
+  getDetails(idResult);
+});
+
+//대기
+function postComment(){
+  const formData = new FormData();
+  formData.append("idResult", idResult);
+  formData.append("commentTitle", commentTitle);
+  formData.append("comment_details", comment_details);
+  $.ajax({
+    type: "POST",
+    url: "/comment",
+    dataType: "json",
+    contentType: false, 
+    processData: false,
+    data: formData,
+    }).done(function (result) {
+      console.log(result);
+    }).fail(function (jqXHR) {
+      console.log(jqXHR);
+    }).always(function () {
+      console.log("실행되는지 확인");
+    });
+}
+
+
+
+
+
+function getDetails(idResult) {
+  fetch(`/api/details/${idResult}`).then((res) =>
+    res.json().then((data) => {
+      let detailResults = data["dataRespone"];
+      let commentReults = data["commentResponse"];
+      console.log(commentReults)
+      detailResults.forEach((detailResult) => {
+        let addrResult = detailResult["주소(도로명)"];
+        let nameResult = detailResult["시장(상점가명)"];
+        let openResult = detailResult["시장유형"];
+        let urlResult = detailResult["홈페이지주소"];
+        let prodResult = detailResult["취급품목"];
+        let storeResult = detailResult["점포수"];
+        let restResult = detailResult["공중화장실보유여부"];
+        let parkResult = detailResult["주차장보유여부"];
+        let detailResult = detailResult['commentId']
+        console.log(urlResult)
+        urlResult = (urlResult === null) ? "준비중" : urlResult;
+        parkResult = (parkResult == "Y")? "有" : "無"
+        restResult = ( restResult == "Y")? "有" : "無"
+
+        console.log(nameResult);
+        let name_html = `<div>${nameResult}</div>`;  
+        $("#nameResult").append(name_html);    
+
+        let addr_html = `<div>${addrResult}</div>`;  
+        $("#addrResult").append(addr_html);  
+
+        let open_html = `<div>${openResult}</div>`;  
+        $("#openResult").append(open_html);  
+
+        let url_html = `<div>${urlResult}</div>`;  
+        $("#urlResult").append(url_html);  
+
+        let prodStore_html = `<div>${prodResult} / ${storeResult}개</div>`;  
+        $("#prodStore").append(prodStore_html);  
+
+        let restPark_html = `<div>${restResult} / ${parkResult}</div>`;  
+        $("#restPark").append(restPark_html);  
+
+
+      
+
+        // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+        var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
+        var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+          mapOption = {
+            center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+            level: 3, // 지도의 확대 레벨
+          };
+
+        // 지도를 생성합니다
+        var map = new kakao.maps.Map(mapContainer, mapOption);
+
+        // 장소 검색 객체를 생성합니다
+        var ps = new kakao.maps.services.Places();
+
+        // 키워드로 장소를 검색합니다
+        ps.keywordSearch(nameResult, placesSearchCB); /////////////////요기
+
+        // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+        function placesSearchCB(data, status, pagination) {
+          if (status === kakao.maps.services.Status.OK) {
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+            // LatLngBounds 객체에 좌표를 추가합니다
+            var bounds = new kakao.maps.LatLngBounds();
+
+            for (var i = 0; i < data.length; i++) {
+              displayMarker(data[i]);
+              bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+            }
+
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+            map.setBounds(bounds);
+          }
+        }
+
+        // 지도에 마커를 표시하는 함수입니다
+        function displayMarker(place) {
+          // 마커를 생성하고 지도에 표시합니다
+          var marker = new kakao.maps.Marker({
+            map: map,
+            position: new kakao.maps.LatLng(place.y, place.x),
+          });
+
+          // 마커에 클릭이벤트를 등록합니다
+          kakao.maps.event.addListener(marker, "click", function () {
+            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+            infowindow.setContent(
+              '<div style="padding:5px;font-size:12px;">' +
+                place.place_name +
+                "</div>"
+            );
+            infowindow.open(map, marker);
+          });
+        }
+      });
+    })
+  );
+}
+
+
+
+
+
+
